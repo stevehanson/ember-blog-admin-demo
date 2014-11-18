907dbfe20a2c87a51cf6af8803e7db1488eb39d6
Steve Hanson <s.hanson5@gmail.com>
2014-11-18 08:35:44 -0600

    Clean up spacing

--- app/assets/javascripts/models/post.js
+++ app/assets/javascripts/models/post.js
@@ -1,5 +1,3 @@
-// for more details see: http://emberjs.com/guides/models/defining-models/
-
 App.Post = DS.Model.extend({
   title: DS.attr('string'),
   body: DS.attr('string'),
--- app/assets/javascripts/router.js
+++ app/assets/javascripts/router.js
@@ -1,4 +1,5 @@
 App.Router.map(function() {
+
   this.resource('users', function() {
     this.route('edit', { path: '/:user_id' });
     this.route('new');
